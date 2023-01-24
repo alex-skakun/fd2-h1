7926d5e8b51a884014be6e2911818f66351b8bfc
Pavel <uscharov@mail.ru>
2023-01-24 21:40:22 +0300

    13092021

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-const userName = prompt('введите ваше имя');
-
+// запускаем цикл проверки имени  ?. давали undefined 
+let userName, checkNumber;
+do {
+userName = prompt('Введите Ваше имя')?.trim();
+} while(!userName);
+ 
 let gender;
-
 do {
     gender = prompt('введите ваш пол (М или Ж)')?.toUpperCase().trim();
 } while (gender !== 'М' && gender !== 'Ж');
-
 alert(`Добро пожаловать, ${gender === 'М' ? 'господин' : 'госпожа'} ${userName}`);
