2e1293f04e545652aadb5028c83815f0e3665c86
Pavel <uscharov@mail.ru>
2023-01-24 21:49:57 +0300

    Поправили код

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+
+
 // запускаем цикл проверки имени  ?. давали undefined 
+
 let userName, checkNumber;
 do {
 userName = prompt('Введите Ваше имя')?.trim();
 } while(!userName);
- 
+
 let gender;
 do {
     gender = prompt('введите ваш пол (М или Ж)')?.toUpperCase().trim();
