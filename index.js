1b9d2b9feb884406985fae257c2823d9295db352
Светалана Курмель <svetalanakurmel@MacBook-Pro-Svetalana.local>
2023-01-24 18:53:27 +0300

    changes for homework

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-const userName = prompt('введите ваше имя');
+let userName;
+
+do {
+    userName = prompt('Введите ваше имя')?.trim();
+} while (!userName);
 
 let gender;
 
 do {
-    gender = prompt('введите ваш пол (М или Ж)')?.toUpperCase().trim();
+    gender = prompt('Введите ваш пол (м или ж)')?.toUpperCase().trim();
 } while (gender !== 'М' && gender !== 'Ж');
 
 alert(`Добро пожаловать, ${gender === 'М' ? 'господин' : 'госпожа'} ${userName}`);
