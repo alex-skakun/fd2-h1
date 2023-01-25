c9568f4c09f435e018277e57f1ce74e70d8275e0
Buten13alex <Butan2018@mail.ru>
2023-01-25 21:16:55 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,4 +6,6 @@ do {
     gender = prompt('введите ваш пол (М или Ж)')?.toUpperCase().trim();
 } while (gender !== 'М' && gender !== 'Ж');
 
+
+
 alert(`Добро пожаловать, ${gender === 'М' ? 'господин' : 'госпожа'} ${userName}`);
