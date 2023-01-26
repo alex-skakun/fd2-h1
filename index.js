0f44477a6916ffee17d7da86f820375b0c427e71
Yanina Matsakova <yanamshow@inbox.ru>
2023-01-26 03:58:43 +0300

    validation

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
-const userName = prompt('введите ваше имя');
 
+const userName = prompt('введите ваше имя');
 let gender;
+
+    if (userName === null) {
+        alert('Вы отказались от ввода') 
+    }
+        else 
+    if (userName.length === 0 || userName === "") {
+        alert('Вы не ввели имя')
+    }
+    
+if (userName.length != 0 || userName != ""){
 
 do {
     gender = prompt('введите ваш пол (М или Ж)')?.toUpperCase().trim();
 } while (gender !== 'М' && gender !== 'Ж');
 
 alert(`Добро пожаловать, ${gender === 'М' ? 'господин' : 'госпожа'} ${userName}`);
+
+} 
+
